86adc1cef161b60d06cc17d2324b31753f3a7d78
Diyorbek <90106883+iDev07@users.noreply.github.com>
2022-07-15 17:38:02 +0500

    My Firts react-native app

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
+const ALL4U = "https://www.all4u.market/";
+const WORLDMIRAL = "https://worldmiral.com";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ width: "100%", height: "100%" }}>
+        <WebView source={{ uri: ALL4U }} onLoad={console.log("loaded")} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +17,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 28,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
